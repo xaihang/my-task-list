c609e54b2db950290b33976d8e5fbf69dff51232
Xai Hang <xaihang12@gmail.com>
2023-02-04 21:13:59 -0600

    updated todo list and clean up codes and comments

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,11 +1,8 @@
 const pg = require('pg');
 
 const pool = new pg.Pool({
-    // the name of the database (songs)
     database: 'weekend-to-do-app',
-    // the location of our database, within the network
     host: 'localhost',
-    // the port number to listen to -- 5432 is the default port
     port: 5432, 
 })
 
